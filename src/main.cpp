3ee667c51c352748bc6c3e11daa7f37ef136e922
chris <christian.schwind@smail.th-koeln.de>
2022-08-20 16:03:48 +0200

    added moving avg for rssi, refactoring, cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <BLEDevice.h>
-//#include <ArduinoHttpClient.h>
 #include <WiFi.h> 
 #include <HTTPClient.h>
+#include <movingAvg.h>
 #include "mySecrets.h"
 //#include "esp_coexist_internal.h"
 //#define CONFIG_SW_COEXIST_ENABLE 1
@@ -11,55 +11,44 @@
 static BLEUUID serviceUUID("91bad492-b950-4226-aa2b-4ede9fa42f59"); 
 static BLEUUID charUUID("91bad492-b950-4226-aa2b-4ede9fa42f59");   
 static BLERemoteCharacteristic *pRemoteCharacteristic;
-String My_BLE_Address = "08:3a:f2:b9:0b:fa";                       
+String Partner_BLE_Address = "b4:e6:2d:eb:17:73";                       
 BLEScan *pBLEScan;
 BLEScanResults foundDevices;
 BLEClient *pClient;
 static BLEAddress *Server_BLE_Address;
 String Scanned_BLE_Address;
 int rssi;
-boolean paired = false; // boolean variable to togge light
+movingAvg rssiAvg(10);
 
 //WIFI
 const char* ssid = SECRET_SSID;
 const char* password = SECRET_PASS;
 std::string serverName = "https://api.openweathermap.org/data/2.5/weather?lat=50.9375&lon=6.9603&appid=5a246f369afff4ce3d85a772dfe8e031";
 
+bool one = true;
+bool two = true;
+unsigned long lastAPICall;
 
 bool connectToServer(BLEAddress pAddress) {
-  pClient = BLEDevice::createClient();
-  Serial.println(" - Created client");
-
-  // Connect to the BLE Server.
   pClient->connect(pAddress);
-  Serial.println(" - Connected to lamp");
-
-  // Obtain a reference to the service we are after in the remote BLE server.
   BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
   if (pRemoteService != nullptr) {
-    Serial.println(" - Found our service");
+    Serial.println("Connected to server");
     return true;
   } else {
+    Serial.println("Connection to server failed");
     return false;
   }
-  /*  // unreachable code. delete?
-  // Obtain a reference to the characteristic in the service of the remote BLE server.
-  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
-  
-  if (pRemoteCharacteristic != nullptr)
-    Serial.println(" - Found our characteristic");
-  return true;
-  */
 }
 
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
   void onResult(BLEAdvertisedDevice advertisedDevice) {
     Serial.printf("Scan Result: %s \n", advertisedDevice.toString().c_str());
-    Server_BLE_Address = new BLEAddress(advertisedDevice.getAddress());
   }
 };
 
-void scanForDevices() {
+void initBLEScan() {
+  BLEDevice::init("");
   Serial.println("ESP32 BLE Server program. Scanning for Bluetooth devices...");
   pBLEScan = BLEDevice::getScan();                                            // create new scan
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());  // Call the class that is defined above
@@ -74,7 +63,7 @@ bool connectWifi() {
   WiFi.begin(ssid, password);
   Serial.print("Attempting to connect to WiFi with following SSID: ");
   Serial.println(ssid);
-  while (counter < 40) {  //if connection cannot be established, attempting ends after 20 seconds (40 * 500ms)
+  while (counter < 40) {  //if connection cannot be established, attempt ends after 20 seconds (40 * 500ms)
     if (WiFi.status() != WL_CONNECTED) {
       delay (500);
       Serial.print(".");
@@ -106,8 +95,9 @@ void getWeather() {
 
 void setup() {
   Serial.begin(115200);
-  BLEDevice::init("");
-  scanForDevices();
+  initBLEScan();
+  pClient = BLEDevice::createClient();
+  rssiAvg.begin();
   pinMode(14, OUTPUT);
   pinMode(26, OUTPUT);
   pinMode(33, OUTPUT);
@@ -115,62 +105,57 @@ void setup() {
 
 
 void loop() {
-  // TODO: First search for device, then call weather. (if too slow: occasionally call weather api (e.g. every 30 min) for quick settings roughly correct)
-  
-  while (paired == false) {
-    foundDevices = pBLEScan->start(3); // Scan for 3 seconds to find the Fitness band
-    Serial.printf("Scan done.\n");
-    Serial.printf("Number of BLE devices found: %d\n", foundDevices.getCount());
+  while (!pClient->isConnected()) {
+    foundDevices = pBLEScan->start(3); // Scan for 3 seconds
+    Serial.printf("\nScan done.\nNumber of BLE devices found: %d\n\n\n)", foundDevices.getCount());
 
     for (int i = 0; i < foundDevices.getCount(); i++) {
       Scanned_BLE_Address = foundDevices.getDevice(i).getAddress().toString().c_str();
-      if (Scanned_BLE_Address == My_BLE_Address && paired == false) {
-        Serial.println("Found Device. Connecting to Server as client");
-        if (connectToServer(foundDevices.getDevice(i).getAddress())) {
-          paired = true;
-          break;
-        } else {
-          Serial.println("Pairing failed");
-          break;
-        }
+      if (Scanned_BLE_Address == Partner_BLE_Address) {
+        Serial.println("Found partner device. Connecting to server as client");
+        connectToServer(foundDevices.getDevice(i).getAddress()) ? Serial.println("Paired with partner device") : Serial.println("Pairing failed");
       }
     }
   }
-  while (paired == true) {
-    if (pClient->isConnected()) {
-      rssi = pClient->getRssi();
-      Serial.printf("RSSI: %d\n", rssi);
-      delay(1000);                  //??????????
-      if (rssi < -80) {
-        Serial.println("LED OFF");
+
+  while (pClient->isConnected()) {
+    rssi = pClient->getRssi();
+    Serial.printf("RSSI: %d       ", rssi);
+    Serial.printf("Moving Average: %d       ", rssiAvg.reading(rssi));
+    Serial.printf("Average input count: %d\n", rssiAvg.getCount());
+    delay(500);
+    if (rssiAvg.getAvg() < -80) {
+      if (one) {
+        one = false;
+        two = true;
+        Serial.println("\nLED OFF\n");
         digitalWrite(14, LOW);
         digitalWrite(26, LOW);
         digitalWrite(33, LOW);
-        break;
-      } else {
-        Serial.println("LED ON");
-        // switch light on with low brightness (default; also settable via interface)
-        if(connectWifi()){
-          getWeather();
-          //WiFi.disconnect();    // TODO:  which one?
-          //WiFi.mode(WIFI_OFF);  //        which one?
-          delay(1000);            // TODO:  how long?
-        }
-        digitalWrite(14, HIGH);
-        digitalWrite(26, HIGH);
-        digitalWrite(33, HIGH);
-        break;
       }
     } else {
-      Serial.println("Disconnected");
-      paired = false;
+      if (two) {
+        two = false;
+        one = true;
+        Serial.println("\nLED ON\n");
+        lastAPICall = millis() - 60000UL;
+        // TODO: switch light on with low brightness (default; also settable via interface)
+        // TODO: if too slow: occasionally call weather api (e.g. every 30 min) for quick settings roughly correct
+      }
+      if (millis() - lastAPICall >= 60000UL){
+        if(WiFi.status() != WL_CONNECTED) 
+          connectWifi();
+        lastAPICall = millis();
+        getWeather();
+        digitalWrite(14, HIGH);   // TODO map readings to lamp settings
+        digitalWrite(26, HIGH);
+        digitalWrite(33, HIGH); 
+      }
+       
     }
   }
-  
-  
-  //pBLEScan->stop();
-  //btStop();
-  Serial.print(5000);
-  delay(6000);
-  //*/
+
+  Serial.println("\nDisconnected\n");
+  WiFi.disconnect();
+  rssiAvg.reset();
 }
