0c16ed775860d6fc7bd0ae2947782a02f97ecc0d
chris <christian.schwind@smail.th-koeln.de>
2022-08-28 08:20:53 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,3 @@
-/*********
-  Rui Santos
-  Complete instructions at https://RandomNerdTutorials.com/esp32-ble-server-client/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*********/
-
 #include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
@@ -12,7 +5,6 @@
 #include <BLE2902.h>
 #include <Wire.h>
 
-//BLE server name
 #define bleServerName "BME280_ESP32"
 #define SERVICE_UUID "91bad492-b950-4226-aa2b-4ede9fa42f59"
 
@@ -34,20 +26,12 @@ class MyServerCallbacks: public BLEServerCallbacks {
 void setup() {
   Serial.begin(115200);
 
-  // Create the BLE Device
   BLEDevice::init(bleServerName);
-
-  // Create the BLE Server
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
-
-  // Create the BLE Service
   BLEService *bmeService = pServer->createService(SERVICE_UUID);
-
-  // Start the service
   bmeService->start();
 
-  // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pServer->getAdvertising()->start();
